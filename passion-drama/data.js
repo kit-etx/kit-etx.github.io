c5a31eee7b8375790a36ceb5c5183c47e205b332
kit-etx <89992207+kit-etx@users.noreply.github.com>
2022-06-29 23:58:29 +0800

    Add temp video player, various layout updates, mobile nav WIP

--- passion-drama/data.js
+++ passion-drama/data.js
@@ -5,7 +5,8 @@ data = [
     title: '獨腳刀',
     titleArt: null,
     subtitle: null,
-    thumbnail: 'https://www.passiontimes.hk/pt/762.jpg',
+    thumbnail: null,
+    coverImage: null,
     desc: '《香港愛情》，三度公演！用舞台為你呈現「新常態」下的香港。\n世上最遠嘅距離，唔係天涯海角，而係一個口罩？\n一枝疫苗針，一條國際線，編織出嚟嘅，到底係一段段異地戀，抑或係一幕幕鬧劇？\n如果有人問你有幾「鐘」意香港？到底佢係考驗你嘅信念，還是考緊你中文？\n動盪歲月裡，每場戀愛都係一場戰疫，喺瘟仔瘟女嘅同時，竟然仲要抗疫！？\n《香港愛情：假裝瘟疫蔓延中》，一同見證瘟疫下的戀情。',
     schedule: null,
     price: null,
@@ -29,17 +30,24 @@ data = [
     title: '香港愛情：假裝瘟疫蔓延中',
     titleArt: null,
     subtitle: null,
-    thumbnail: 'https://www.passiontimes.hk/pt/762.jpg',
+    thumbnail: 'https://www.passiontimes.hk/pt/hklove2021.jpg',
+    coverImage: 'https://www.passiontimes.hk/pt/hklove2021.jpg',
     desc: '《香港愛情》，三度公演！用舞台為你呈現「新常態」下的香港。\n世上最遠嘅距離，唔係天涯海角，而係一個口罩？\n一枝疫苗針，一條國際線，編織出嚟嘅，到底係一段段異地戀，抑或係一幕幕鬧劇？\n如果有人問你有幾「鐘」意香港？到底佢係考驗你嘅信念，還是考緊你中文？\n動盪歲月裡，每場戀愛都係一場戰疫，喺瘟仔瘟女嘅同時，竟然仲要抗疫！？\n《香港愛情：假裝瘟疫蔓延中》，一同見證瘟疫下的戀情。',
     schedule: null,
     price: {
       hkd: 190,
-      caption: '附贈 200 點 Zeiton / PRIME 用戶免費收看',
+      caption: '現購買附贈 200 點 Zeiton',
     },
     primeAccess: true,
     ep: {
-      1: 'https://ptmirror.passiontimes.hk/cache/20190323_762/hls/012_A_HD_Mid.mp4_master.m3u8',
-      2: 'https://ptmirror.passiontimes.hk/cache/20190323_762/hls/013_B_HD_Mid.mp4_master.m3u8',
+      1: {
+        source:
+          'https://ptmirror.passiontimes.hk/cache/HK_Love_Story_Pretend_The_Plague_is_Spreading/hls/HK_Love_Story_Pretend_The_Plague_is_Spreading.mp4_master.m3u8',
+        thumbnail: 'https://www.passiontimes.hk/pt/hklove2021.jpg',
+        title: '假裝瘟疫蔓延中',
+        desc: '《香港愛情》，三度公演！用舞台為你呈現「新常態」下的香港。',
+        duration: '2:08:41',
+      },
     },
     year: 2021,
     host: ['762'],
@@ -56,7 +64,8 @@ data = [
     title: '戀愛不是請客吃飯',
     titleArt: null,
     subtitle: null,
-    thumbnail: 'https://www.passiontimes.hk/pt/762.jpg',
+    thumbnail: '/img/17-thumbnail.jpg',
+    coverImage: 'https://www.passiontimes.hk/pt/762.jpg',
     desc: '陳秀慧原著小說，黃洋達改編音樂劇、法子擔綱演出，更推出超大尺度寫真！\n再加上 Eric Chow 編繪漫畫，762 與阿 A 原創歌曲。\n音樂劇方面更有小夜、馬欣澄、Anson、阿 A 加盟參演！\n\n小說、舞臺、寫真、漫畫、音樂，集於一身！ 挑戰創意極限。',
     schedule: { time: '22:00', days: ['Sunday'] },
     price: {
@@ -130,6 +139,7 @@ data = [
     titleArt: null,
     subtitle: null,
     thumbnail: 'https://www.passiontimes.hk/pt/pt8th.jpg',
+    coverImage: 'https://www.passiontimes.hk/pt/pt8th.jpg',
     schedule: null,
     desc: '黃洋達小說《紅樓夢想》正式搬上舞台！\n發掘文學經典，全新劇本，全新創作廣東歌，重塑我們的紅樓夢想音樂劇！\n一個舞台，論盡香港情愛。',
     price: {
@@ -169,6 +179,7 @@ data = [
     titleArt: null,
     subtitle: null,
     thumbnail: 'https://www.passiontimes.hk/images/hklove1111.png',
+    coverImage: 'https://www.passiontimes.hk/images/hklove1111.png',
     schedule: null,
     desc: '一個舞台，論盡香港情愛。\n亂世中的香港，烽火裡的情鴛。動盪的年月裡，香港地每一場戀愛，都是一場奮戰。\n你愛香港，必須為所愛而戰，你要在香港求愛，更要賭上一切！\n熱血七週年呈獻，《台慶劇場：香港愛情》 黃洋達、阿 A、日新、大力、Anson、762、湘傑、Maki、Eric Chow、雞毛、法子、陳四、VVN、打令、阿悠，傾力演出！',
     price: {
@@ -215,6 +226,7 @@ data = [
     titleArt: null,
     subtitle: null,
     thumbnail: 'https://sc6.passiontimes.hk/cache/hklove929/thumbnail.jpg',
+    coverImage: 'https://sc6.passiontimes.hk/cache/hklove929/thumbnail.jpg',
     schedule: null,
     desc: '跟香港文化，談一場戀愛。\n黃洋達、阿 A、Eric、762，擕手共演，用歌曲作畫，同時用畫筆歌唱，在舞台上說唱一個關於香港的愛情故事。\n由安娜瑪德蓮娜唱到甜蜜蜜，再由紫釵記演到秋天的童話。\n這一晚，跨越香港不同時代談情說愛。',
     price: {
@@ -241,6 +253,7 @@ data = [
     titleArt: null,
     subtitle: null,
     thumbnail: 'https://www.passiontimes.hk/pt/762.jpg',
+    coverImage: 'https://www.passiontimes.hk/pt/762.jpg',
     schedule: null,
     desc: '憑「粵語作曲法」獨步天下的《熱血時報》午間皇牌節目〈國立大台〉台柱 762，今年除推出《762 國立精選：香港冇事》之餘，更舉行了專輯同名個人演唱會！\n762 每星期於節目以粵語即席譜寫歌曲，唱盡本地國際乃自於個人的大小事，由核事故防災教學、韓國風潮，到本地政經、傳媒道德，以及社交媒體與個人感受。\n2019 年大家一齊無視全線西冧，細聽 762 如何以好歌唱好香港！',
     price: {
@@ -268,6 +281,7 @@ data = [
     titleArt: null,
     subtitle: '棟篤·笑死朕2018',
     thumbnail: 'https://www.passiontimes.hk/pt/762.jpg',
+    coverImage: 'https://www.passiontimes.hk/pt/762.jpg',
     schedule: null,
     desc: '傳說，只要收集到七個港豬，宇宙中一切嘅衝突同糾紛，都可打和 super！一個因為沒有頭髮而無法變成超西的西人，來到香港，尋找七個港豬，新的傳說，就此展開！',
     price: {
@@ -295,6 +309,7 @@ data = [
     titleArt: null,
     subtitle: '熱血時報 6 週年台慶',
     thumbnail: 'https://www.passiontimes.hk/pt/762.jpg',
+    coverImage: 'https://www.passiontimes.hk/pt/762.jpg',
     schedule: null,
     desc: '熱血時報 6 週年台慶，黃洋達將《金錢師》搬上舞台，全台精英盡出，以說書、演唱、舞蹈、劇場的綜合表演方式，於舞台上嶄新演繹完全不同面貌的金錢師！',
     price: {
@@ -322,6 +337,7 @@ data = [
     titleArt: null,
     subtitle: null,
     thumbnail: 'https://www.passiontimes.hk/pt/762.jpg',
+    coverImage: 'https://www.passiontimes.hk/pt/762.jpg',
     schedule: null,
     desc: '2012 年，黃洋達監倉度橋，6 月在九展演講廳，舉辦 3 場「新九龍皇帝」talk show。\n「網民叫我做皇上，我又會喺九龍東參選，仲唔係九龍皇帝……嗱！唔係抽曾灶財水，但佢啲 graffiti（指墨寶）其實宣示緊主權，我哋咪用 graffiti 宣示番人民主權」。\nTalk show 取材自服刑期間的監倉生活，「廁所搞屎橋就聽得多，監倉橋都算係一個 point！」依家只需要港幣 120 元，即可無限次重溫當晚精彩演出！而參加咗《熱血主持育成計劃》嘅「貴族金主 」，更加可以免費無限次觀賞添～',
     price: {
@@ -349,6 +365,7 @@ data = [
     titleArt: null,
     subtitle: null,
     thumbnail: 'https://www.passiontimes.hk/pt/762.jpg',
+    coverImage: 'https://www.passiontimes.hk/pt/762.jpg',
     schedule: null,
     desc: '2013 年，《熱血時報》一周年，你去咗邊？\n呢一年，黃洋達聯同陳秀慧，《百無禁技》主持阿 Frank，仲有神奇膠樂隊就去咗搞「花生將」talk show，唔止大派花生，仲大放笑彈，又唱又跳，為大家帶來一個輕鬆又愉快的晚上。\n想睇返佢哋當晚嘅精彩演出？依家只需要港幣 120 元，即可無限次重溫！而參加咗《熱血主持育成計劃》嘅「貴族金主 」，更加可以免費無限次觀賞添～\n細數政黨分裂，講到人格分裂；鳩做 TALK SHOW，大把野撈！《熱血時報》「花生將」，送禮自奉兩相宜！',
     price: {
@@ -376,6 +393,7 @@ data = [
     titleArt: null,
     subtitle: null,
     thumbnail: 'https://www.passiontimes.hk/pt/762.jpg',
+    coverImage: 'https://www.passiontimes.hk/pt/762.jpg',
     schedule: null,
     desc: '2017 年 11 月 11 日，熱血時報迎來第 5 週年台慶，今年一眾主持人進佔九展 Music Zone 舉行台慶匯演。\n比起往年，今年的匯演更大規模、更專業、更精彩，一眾主持人施展渾身解數，與觀眾一同度過一個精彩的晚上。\n想重溫當晚精彩演出？想見證新一屆課金王登基及 Miss Hotdog 加冕？現在有得重溫喇！',
     price: {
@@ -403,6 +421,7 @@ data = [
     titleArt: null,
     subtitle: '國立大台300集',
     thumbnail: 'https://www.passiontimes.hk/pt/762.jpg',
+    coverImage: 'https://www.passiontimes.hk/pt/762.jpg',
     schedule: null,
     desc: '本台午間節目《國立大台》在 6 月 29 日播出第 300 集，同日晚上舉行的聚會，除了是黃洋達新作《聰明人類笑死朕》分享會之外，同時是主持 762 的金曲巡禮。\n762 將過往在《國立大台》即場創作的 35 首歌曲重新演繹，令人耳目一新。\n如今只需$120 即可在網上重溫 762 當晚的精彩演出，而參與了《熱血主持育成計劃》的「貴族金主」更可免費收看。',
     price: {
@@ -430,6 +449,7 @@ data = [
     titleArt: null,
     subtitle: null,
     thumbnail: 'https://www.passiontimes.hk/pt/762.jpg',
+    coverImage: 'https://www.passiontimes.hk/pt/762.jpg',
     schedule: null,
     desc: '2016 年 11 月 11 日，《熱血時報》製作了成立 4 周年來首個台慶 show，當晚《熱血時報》總動員出擊，節目多元化，武術、歌舞、話劇、共冶一爐，演出者為台慶竭盡全力，絕不馬虎。\n當晚小龍如何成為台慶課金王？第一屆 Miss Hot Dog 如何誕生？大家想重溫當晚一眾主持的精彩演出，現在就可以付費重溫。',
     price: {
@@ -457,6 +477,7 @@ data = [
     titleArt: null,
     subtitle: '二犬十一咪 ╳ 熱血劇場（006）',
     thumbnail: 'https://www.passiontimes.hk/pt/762.jpg',
+    coverImage: 'https://www.passiontimes.hk/pt/762.jpg',
     schedule: null,
     desc: '關於動物維權動物事，我們運用劇場去感染你。\n我們有 Bonix 扮演外星人養小人，與二犬十一咪互動保護弱小、\n鄭松泰運用巨型男女人偶，販賣神犬拉西？！\nOlive 帶領黃牛一族，借施肥道出辛勞的一生…\n還有一條海豚, 兩隻唐狗，巨星雪碧哥，\n和嵐嵐小朋友，一眾外星人及牛居民、\n串埋一起，用輕鬆方法告訴你感到切膚之痛的動物事！',
     price: {
@@ -484,6 +505,7 @@ data = [
     titleArt: null,
     subtitle: null,
     thumbnail: 'https://www.passiontimes.hk/pt/762.jpg',
+    coverImage: 'https://www.passiontimes.hk/pt/762.jpg',
     schedule: null,
     desc: '一個神秘的失蹤事件，引發連串的奇奧連鎖。這是一個港版費城實驗？抑或只是一段逃情錯愛？\n《奇奧研究社》主持 Kiyo 親自編寫與執導，本港首部結合神秘學、陰謀論及愛情故事的多媒體劇作！費\n城實驗、蒙托奇計劃、MKUltra、人工智能、香港諜報活動等多個元素，為難逃被捕獵的年青戀人們，譜出堅定的戀情挽歌！\n\n著名物理學博士陳志宏、科幻小說家夏霽友情參演！\n《神秘之夜》主持人梁錦祥、《MJ13》主持人卓飛全力支持！',
     price: {
@@ -511,6 +533,7 @@ data = [
     titleArt: null,
     subtitle: null,
     thumbnail: 'https://www.passiontimes.hk/pt/762.jpg',
+    coverImage: 'https://www.passiontimes.hk/pt/762.jpg',
     schedule: null,
     desc: '萬人佔領中環，爭取普選！係咪真係咁堅？\n一個不會破壞秩序嘅佔領行動，同一個有貞節牌坊嘅妓女，有咩關係？\n佔領四部曲，到底係咩？係咪「和‧理‧非‧非」？\n到時會變左民主聖戰？定係 K 歌之王爭霸戰？\n熱血劇場，率先為你一一預演！\n\n「勇武樂壇、撤退唔難！反正得閒，佔領中環。」',
     price: {
@@ -555,6 +578,7 @@ data = [
     titleArt: null,
     subtitle: null,
     thumbnail: 'https://www.passiontimes.hk/pt/762.jpg',
+    coverImage: 'https://www.passiontimes.hk/pt/762.jpg',
     schedule: null,
     desc: '香港人除了政治冷感，還有…性冷感？由熱血時報人氣網台節目《百無禁技》主持 Frank 聯同一眾痴男怨女，攜手演出破格愛慾劇場。拒絕一切壓情禁色！帶你進入愛慾迷宮，情色互動，打破禁忌。\n\nFrank：「一套可以睇到你一齊出野！出櫃！出竅！出神入化嘅表演！」',
     price: {
